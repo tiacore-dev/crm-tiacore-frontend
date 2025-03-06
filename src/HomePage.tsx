de30062b68a8d28f8cf0821e38af804a193c676e
wsrv <cherdantseva.mar@gmail.com>
2025-03-06 17:31:36 +0700

    get req

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -3,34 +3,34 @@ import { useQuery } from "@tanstack/react-query";
 import refreshToken from "./auth";
 import axiosInstance from "./axiosConfig";
 
-// const fetchUserData = async () => {
-//   const url = process.env.REACT_APP_API_URL;
+const fetchUserData = async () => {
+  const url = process.env.REACT_APP_API_URL;
 
-//   const accessToken = localStorage.getItem("access_token");
+  const accessToken = localStorage.getItem("access_token");
 
-//   const rersponse = await axiosInstance.get(
-//     `${url}/api/get-all/legal-entity-types`,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//       },
-//     }
-//   );
-//   return rersponse.data;
-// };
+  const response = await axiosInstance.get(
+    `${url}/api/get-all/legal-entity-types`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+  return response.data;
+};
 
 const HomePage: React.FC = () => {
   // const [shouldFetch, setShouldFetch] = useState(false); //
-  // const { data, isLoading, isError, refetch } = useQuery({
-  //   queryKey: ["userData"],
-  //   queryFn: fetchUserData,
-  //   // enabled: shouldFetch,
-  // });
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["userData"],
+    queryFn: fetchUserData,
+    // enabled: shouldFetch,
+  });
 
-  // const handleFetchData = () => {
-  //   // setShouldFetch(true); // Включаем запрос
-  //   refetch(); // Выполняем запрос
-  // };
+  const handleFetchData = () => {
+    // setShouldFetch(true); // Включаем запрос
+    refetch(); // Выполняем запрос
+  };
 
   const tryRefresh = () => {
     refreshToken();
@@ -39,14 +39,14 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Вы успешно авторизовались!</h1>
-      {/* <button onClick={handleFetchData}> данные?</button>
+      <button onClick={handleFetchData}> данные?</button>
 
       {data && (
         <div>
           <h2>Ваши данные:</h2>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
-      )} */}
+      )}
 
       <button onClick={tryRefresh}>обновить токен</button>
     </div>
