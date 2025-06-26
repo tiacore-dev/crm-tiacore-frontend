fa9d290038b940445de64750fe7253d70ab3cad5
wsrv <wsrvwsrvwsrv@gmail.com>
2025-06-26 22:28:09 +0700

    404+*

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ const App: React.FC = () => {
                       path="/templates/:template_id"
                       element={<TemplateDetailsPage />}
                     />
-                    <Route path="/404" element={<NotFoundPage />} />
+                    <Route path="/*" element={<NotFoundPage />} />
                   </Route>
                   <Route path="*" element={<Navigate to="/login" />} />
                 </Routes>
